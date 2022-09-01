a609d2883731de7d636f91d902230e69220c317c
Michael Binnick <mbinnick@live.ca>
2022-09-01 21:54:51 +0000

    refactored breedFetcher to export functionality and utilize callbacks, wrote index.js to handle client-facing interactions and import breedFetcher functionality, and wrote breedFetcherTests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,25 @@
 const request = require('request');
 
-const args = process.argv.slice(2);
 
-// const query
-const url = "https://api.thecatapi.com/v1/breeds";
-// const path = "/search" + query;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const data = JSON.parse(body);
-  for (const breed of data) {
-    if (breed.name === args[0]) {
-      console.log(breed.description);
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  
+  request(url, (error, response, body) => {
+    
+    if (error) {
+      callback(error, null);
       return;
     }
-  }
-  console.log('Error: breed not found!');
-});
+    
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback('Breed not found?!', null);
+      return;
+    }
+
+    callback(null, data[0].description);
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
